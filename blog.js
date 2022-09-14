78c5303abb1b1b09d73f5517225285b88dad7fdd
AvinavStha <avi.shrestha.775@gmail.com>
2022-09-14 14:19:39 +0545

    backToTop fixed

--- blog.js
+++ blog.js
@@ -1,18 +1,16 @@
-let backToTop = document.getElementById("myBtn");
-
-// When the user scrolls down 20px from the top of the document, show the button
-window.onscroll = function() {scrollFunction()};
+const toTop = document.querySelector("#back-to-top");
+window.addEventListener("scroll", scrollFunction);
 
 function scrollFunction() {
-  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-    backToTop.style.display = "block ";
+  if (window.pageYOffset > 400) {
+    toTop.style.display = "block";
   } else {
-    backToTop.style.display = "none ";
+    toTop.style.display = "none";
   }
 }
 
-// When the user clicks on the button, scroll to the top of the document
-function topFunction() {
-  document.body.scrollTop = 0;
-  document.documentElement.scrollTop = 0;
+toTop.addEventListener("click", backToTop);
+
+function backToTop() {
+  window.scrollTo(0, 0);
 }
